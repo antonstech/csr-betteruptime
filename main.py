c5906b8598a2d56cc1790a0251147883aa8a77a4
Anton <58666027+antonstech@users.noreply.github.com>
2021-07-04 14:23:49 +0200

    Now on the Status Page itselfs the Name updates too

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ heartbeaturl = config["Webhook-Ping"]["url"]
 api_token = config["API-Stuff"]["token"]
 server_name = config["Device-Info"]["name"]
 heartbeatid = config["API-Stuff"]["id"]
-api = config["API-Stuff"]["url"]
+heartbeat_api = config["API-Stuff"]["heartbeat_url"]
+stupdateurl = config["API-Stuff"]["statuspage_url"]
+statuspage_id = config["API-Stuff"]["statuspage_id"]
+resource_id = config["API-Stuff"]["resource_id"]
 
 requests.get(heartbeaturl)
 
-full_url = api + str(heartbeatid)
+full_url = heartbeat_api + str(heartbeatid)
 get_free_space = shutil.disk_usage("/")[2]
 free_space = ("%d GB Frei" % (get_free_space // (2 ** 30)))
 data = {"name" : f"{server_name} ({free_space})"}
 headers = {"Authorization": f"Bearer {api_token}"}
 response = requests.patch(full_url, data=data, headers=headers).json()
+status_page_update_url = stupdateurl + statuspage_id + "/resources/" + resource_id
+print(status_page_update_url)
+data = {"public_name" : f"{server_name} ({free_space})"}
+response2 = requests.patch(status_page_update_url, data=data, headers=headers).json()
+print(response2)
 
 
